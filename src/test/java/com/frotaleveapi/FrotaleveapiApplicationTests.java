da25a6da29eeefe3f87ebbd69b30762a20d3f63c
diego.terto <diego.terto@db1.com.br>
2024-08-26 21:16:44 -0300

    first commit

---
+++ src/test/java/com/frotaleveapi/FrotaleveapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frotaleveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrotaleveapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
